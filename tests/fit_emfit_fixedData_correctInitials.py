bcf43f6c66ca132e64e7c22d6901d83fdd554af9
Wolfgang Mader <Wolfgang.Mader@fdm.uni-freiburg.de>
2015-05-11 18:09:04 +0200

    Add test starting with correct parameter matrix A.

--- tests/fit_emfit_fixedData_correctInitials.py
+++ tests/fit_emfit_fixedData_correctInitials.py
@@ -24,10 +24,13 @@ class MleRes:
 # Data parameters
 dim = 2
 order = 3
-nData = 30000
+nData = 300
 aThresh = 1e-7
 pThresh = 1e-7
 maxIter = 100000
+
+trueA = np.array([[0.9000, 0,      -0.5000,  0.1000,  0,       0.1500],
+                  [0.3500, 0.7000,  0.2000, -0.3000, -0.2500, -0.4000]])
 
 # True parameters
 # A1 =  0.9000         0
@@ -63,10 +66,10 @@ lsA, lsQ, lsR = fdmb.arfit(hiddenP5, nData, dim, order)
 # Fit VAR model to data, EM
 print('Start EM on SNR=1/2')
 mleP5A, mleP5Q, mleP5R, mleP5X, errA, errQ, errR \
-      = fdmb.emfit(obsP5, nData, dim, order, aThresh, pThresh, maxIter, False)
-print('Start EM on SNR=1/8')
-mleP125A, mleP125Q, mleP125R, mleP125X, errA, errQ, errR \
-      = fdmb.emfit(obsP125, nData, dim, order, aThresh, pThresh, maxIter, False)
+      = fdmb.emfit(obsP5, nData, dim, order, aThresh, pThresh, maxIter, False, trueA)
+#print('Start EM on SNR=1/8')
+#mleP125A, mleP125Q, mleP125R, mleP125X, errA, errQ, errR \
+      #= fdmb.emfit(obsP125, nData, dim, order, aThresh, pThresh, maxIter, False, trueA)
 
 mleResList = []
 mleResList.append(MleRes())
@@ -79,15 +82,15 @@ mleResList[-1].pThresh = pThresh
 mleResList[-1].SNR = .5
 mleResList[-1].nData = nData
 
-mleResList.append(MleRes())
-mleResList[-1].mleA = mleP125A
-mleResList[-1].mleQ = mleP125Q
-mleResList[-1].mleR = mleP125R
-mleResList[-1].mleX = mleP125X
-mleResList[-1].aThresh = aThresh
-mleResList[-1].pThresh = pThresh
-mleResList[-1].SNR = 1/8
-mleResList[-1].nData = nData
+#mleResList.append(MleRes())
+#mleResList[-1].mleA = mleP125A
+#mleResList[-1].mleQ = mleP125Q
+#mleResList[-1].mleR = mleP125R
+#mleResList[-1].mleX = mleP125X
+#mleResList[-1].aThresh = aThresh
+#mleResList[-1].pThresh = pThresh
+#mleResList[-1].SNR = 1/8
+#mleResList[-1].nData = nData
 
 
 # Write results to disk
